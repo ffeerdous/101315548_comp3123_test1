f66cf5e7b61eaba8d3d029f93a3f2a2cc5e28661
ffeerdous <ferdousazizi9@gmail.com>
2022-10-06 19:10:24 -0400

    New Changes

--- Question-3/fileModule.js
+++ Question-3/fileModule.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path")
 
  function createLogs(){
     const dir = "./logs";
@@ -6,7 +7,7 @@ const fs = require("fs");
         fs.mkdirSync(dir)
     }
  process.chdir(dir);
- for (let i = 0; i < 10; i++){
+ for (let i = 0; i <= 10; i++){
     fs.writeFile(`log${i}.txt`, 'This is a Log File', (error) =>
     error?
     console.log(`Error creating file 'log${i}.txt'`) : console.log(`creating file 'log${i}.txt'`)
@@ -16,16 +17,22 @@ const fs = require("fs");
 
 function removeLogs(){
     const dir = "./logs";
-    if (fs.existsSync(dir)) {
-        process.chdir(dir);
-        fs.readdir(process.cwd(), (err, files) => {
-        files.forEach((file) => {
-            fs.unlink(file, (err) =>
-            err ? console.error(`Error deleting file '${file}'`) : console.log(`Deleting file '${file}'`)
-            );
-        });
+    fs.readdir(dir, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files){
+            fs.unlink(path.join(dir, file), (err) => {
+                if (err) throw err;
+                console.log(`Deleting file '${file}'`)
+            })
+        }
     });
-    }
+    fs.rmdir(dir, (err) => {
+        if (err){
+            console.log("error occured in deleting folder", err);
+        }
+        console.log("directory deleted successfully");
+    })
 };
-createLogs();
-//removeLogs();
+//createLogs();
+removeLogs();
